9d7244917018c9386aaee07bb3ee117c7f30beab
ancelcruzch <ancel.cruz@ucsp.edu.pe>
2023-09-06 18:20:23 -0500

    inicializando el proyecto - backend

---
+++ server/constants.js
@@ -0,0 +1,14 @@
+const DB_USER = "admin";
+const DB_PASSWORD = "admin12345"; 
+const DB_HOST = "personal-web.wb6ilvc.mongodb.net"; 
+
+const API_VERSION = "v1";
+const IP_SERVER = "localhost";
+
+module.exports = {
+    DB_USER,
+    DB_PASSWORD,
+    DB_HOST,
+    API_VERSION,
+    IP_SERVER
+};
