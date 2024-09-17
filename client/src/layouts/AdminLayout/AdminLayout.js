921fee34f0efd8db9c0998580fc590ab6fc3d765
Alain Cruz <acruzch@Alains-MacBook-Air.local>
2024-09-17 17:01:09 -0500

    panel de administracion - frontend

--- client/src/layouts/AdminLayout/AdminLayout.js
+++ client/src/layouts/AdminLayout/AdminLayout.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from "react";
+import { Icon } from "../../assets";
+import { AdminMenu, Logout } from "../../components/Admin/AdminLayout";
+import "./AdminLayout.scss";
 
 export function AdminLayout(props) {
-    const {children} = props;
-    return (
-        <div>
-            <h2>Se esta usando el AdminLayout</h2>
-            {children}
+  const { children } = props;
+
+  return (
+    <div className="admin-layout">
+      <div className="admin-layout__left">
+        <Icon.LogoWhite className="logo" />
+        <AdminMenu />
+      </div>
+      <div className="admin-layout__right">
+        <div className="admin-layout__right-header">
+          <Logout />
         </div>
-    )
-}
+        <div className="admin-layout__right-content">{children}</div>
+      </div>
+    </div>
+  );
+};
