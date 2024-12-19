6d8c821d1c32c5e3b4850ee18f203c48cd5f2b74
Alain Cruz <acruzch@Alains-MacBook-Air.local>
2024-12-19 11:53:41 -0500

    sistema de newsletter - frontend

--- client/src/pages/admin/Newsletter/Newsletter.js
+++ client/src/pages/admin/Newsletter/Newsletter.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React from "react";
+import { Tab } from "semantic-ui-react";
+import { ListEmails } from "../../../components/Admin/Newsletter";
 
 export function Newsletter() {
+  const panes = [
+    {
+      render: () => (
+        <Tab.Pane attached={false}>
+          <ListEmails />
+        </Tab.Pane>
+      ),
+    },
+  ];
+
   return (
-    <div>
-      <h1>Estamos en Newsletter</h1>
+    <div className="newsletter-page">
+      <Tab menu={{ secondary: true }} panes={panes} />
     </div>
-  )
+  );
 }
